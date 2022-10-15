cca0127f7c4725c8f7183047a38c81c9bbdba672
Valss22 <valsshokorov@gmail.com>
2022-10-16 00:14:14 +0600

    Delete room tortoise model

--- src/app/message/model.py
+++ src/app/message/model.py
@@ -1,7 +1,1 @@
-from tortoise import fields, models
 
-class Message(models.Model):
-    id = fields.IntField(pk=True)
-    text = fields.TextField()
-    user = fields.ForeignKeyField('models.User', related_name='message')
-    room = fields.ForeignKeyField('models.Room', related_name='message')
--- src/app/user/model.py
+++ src/app/user/model.py
@@ -8,5 +8,4 @@ users = sqlalchemy.Table(
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column('name', sqlalchemy.String(255), unique=True),
     sqlalchemy.Column('password', sqlalchemy.String(255)),
-    sqlalchemy.Column('message', sqlalchemy.Integer, sqlalchemy.ForeignKey('messages.id'))
 )
